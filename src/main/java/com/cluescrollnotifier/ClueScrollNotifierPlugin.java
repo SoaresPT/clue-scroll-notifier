f1e9162545e1555b224f16c71ed143346972a3d6
Soares <soares_123@msn.com>
2024-05-28 13:01:57 +0300

    New menu options and more robust message detection

--- src/main/java/com/cluescrollnotifier/ClueScrollNotifierPlugin.java
+++ src/main/java/com/cluescrollnotifier/ClueScrollNotifierPlugin.java
@@ -6,13 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.ItemSpawned;
 import net.runelite.client.Notifier;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.woodcutting.config.ClueNestTier;
 import okhttp3.OkHttpClient;
-
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -20,8 +21,8 @@ import java.util.concurrent.ThreadLocalRandom;
 @PluginDescriptor(
 		name = "Clue Scroll Notifier"
 )
-public class ClueScrollNotifierPlugin extends Plugin {
 
+public class ClueScrollNotifierPlugin extends Plugin {
 	@Inject
 	private Client client;
 
@@ -40,13 +41,14 @@ public class ClueScrollNotifierPlugin extends Plugin {
 	@Inject
 	private Notifier notifier;
 
+	private boolean birdNestFallen;
+
 	@Override
 	protected void startUp() throws Exception {
 		executor.submit(() -> {
 			FileManager.ensureDownloadDirectoryExists();
 			FileManager.downloadAllMissingSounds(okHttpClient);
 		});
-
 		log.info("ClueScrollNotifier started!");
 	}
 
@@ -59,23 +61,55 @@ public class ClueScrollNotifierPlugin extends Plugin {
 	@Subscribe
 	public void onChatMessage(ChatMessage chatMessage) {
 		ChatMessageType type = chatMessage.getType();
+		String message = chatMessage.getMessage().toLowerCase();
 
-		if ((type == ChatMessageType.GAMEMESSAGE || type == ChatMessageType.SPAM)
-				&& chatMessage.getMessage().toLowerCase().contains("clue scroll")) {
+		if (type == ChatMessageType.GAMEMESSAGE && message.contains("Untradeable drop: Clue scroll".toLowerCase())) {
+			if (config.notifyClueScrollDrops()) {
+				notify("Clue scroll found!");
+			}
+		}
+
+		if (type == ChatMessageType.SPAM && message.contains("You steal a clue scroll".toLowerCase())) {
+			if (config.notifyPickpockets()) {
+				notify("Clue scroll found!");
+			}
+		}
+
+		if (message.contains("a bird's nest falls out of the tree")) {
 			log.info("Received chat message: '{}' of type {}", chatMessage.getMessage(), type);
+			birdNestFallen = true;
+		}
+	}
 
-			if (config.playSound()) {
-				playRandomClueScrollSound();
+	@Subscribe
+	public void onItemSpawned(ItemSpawned itemSpawned) {
+		if (birdNestFallen) {
+			ClueNestTier clueTier = ClueNestTier.getTierFromItem(itemSpawned.getItem().getId());
+			if (clueTier != null) {
+				log.info("Clue Spawned {}", clueTier);
+				if (config.notifyClueNests()) {
+					notify("A bird's nest with a clue has fallen out of the tree!");
+				}
 			}
-			if (config.showNotification()) {
-				notifier.notify("Clue scroll found!");
-			}
+
+			birdNestFallen = false; // Reset the flag
+		}
+	}
+
+
+
+	private void notify(String message) {
+		if (config.playSound()) {
+			playRandomClueScrollSound();
+		}
+
+		if (config.showNotification()) {
+			notifier.notify(message);
 		}
 	}
 
 	private void playRandomClueScrollSound() {
 		int whichSound = ThreadLocalRandom.current().nextInt(1, 3);
-
 		switch (whichSound) {
 			case 2:
 				log.info("Playing CLUE2 sound.");
